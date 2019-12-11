3093c0dca83bccc14b0c51f5a41210155dc72771
Serial Lab <slab@serial-lab.com>
2019-12-11 11:39:54 -0700

    adding the request audit log

--- quartet_vrs/models.py
+++ quartet_vrs/models.py
@@ -21,3 +21,39 @@ class VRSGS1Locations(models.Model):
 
     class Meta:
         db_table = 'quartet_vrs_glns'
+
+
+class VRSRequest(models.Model):
+    GTIN14 = models.CharField(max_length=14,
+                              null=False,
+                              blank=False,
+                              help_text=_('The GTIN 14 from the request.'),
+                              verbose_name=_('GTIN 14'))
+    lot = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False,
+        help_text=_('Lot/Batch'),
+        verbose_name=_('Lot/Batch')
+    )
+    expiry = models.DateField(
+        null=False,
+        blank=False,
+        verbose_name=_('Expiration Date'),
+        help_text=('The expiration date of the item being verified.')
+    )
+    serial_number = models.CharField(
+        null=False,
+        blank=False,
+        verbose_name=_('Serial Number')
+    )
+    ip_address = models.IPAddressField(
+        blank=True,
+        verbose_name=_('IP Address'),
+        help_text=('The source IP address.')
+    )
+    header = models.TextField(
+        blank=True,
+        verbose_name=_('Request Header'),
+        help_text=_('The full HTTP request header.')
+    )
